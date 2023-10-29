3abe69d0dec0ec0fae8c545de39831da3d76a6d4
AdrianVargas23 <adrianpatata23@live.com>
2023-10-28 19:41:03 -0700

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+import 'package:firebase_auth/firebase_auth.dart' as firebase_auth;
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  MyApp({Key? key}) : super(key: key);
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  firebase_auth.FirebaseAuth firebaseAuth = firebase_auth.FirebaseAuth.instance;
+
+  void signup() async {
+    try {
+      await firebaseAuth.createUserWithEmailAndPassword(
+          email: "adrianpatata23@gmail.com", password: "123456");
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Mandy"),
+        ),
+        body: Center(
+            child: ElevatedButton(
+          onPressed: () {
+            signup();
+          },
+          child: Text("Iniciar sesión"),
+        )),
+      ),
+    );
+  }
+}
